8499b59b919d2e8fe5d3e4ef677240fa8c11be40
Cezar Cretu <cretucezarandrei@gmail.com>
2024-09-15 11:57:30 +0300

    start on amef

---
+++ Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Vatee;
+
+public class Logger
+{
+    private static readonly Lazy<bool> IsDebug =
+        new(() => !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("VATEE_DEBUG_PATH")));
+
+    public static void WriteLine(string line)
+    {
+        if (!IsDebug.Value)
+        {
+            return;
+        }
+
+        Console.WriteLine();
+    }
+}
